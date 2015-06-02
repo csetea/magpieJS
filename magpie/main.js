6171510264e18513a52acf1ac605ca3c92b6af72
csetea <csetea@ancs-ubuntu>
2015-06-02 17:01:57 +0200

    added version info

--- magpie/main.js
+++ magpie/main.js
@@ -3,10 +3,11 @@
  * @license MIT
  */
 define([],{
-	name:'magpieJS',
-	desc: 'AMD module collection packet',
-	author: 'András Csete',
-	URL: 'https://github.com/csetea/magpieJS',
-	license: 'MIT',
-	licenseURL: 'https://github.com/csetea/magpieJS/blob/master/LICENSE.md',
+	name:		'magpieJS',
+	desc: 		'AMD module collection packet',
+	author: 	'András Csete',
+	URL: 		'https://github.com/csetea/magpieJS',
+	license:	'MIT',
+	version: 	'0.3',
+	licenseURL:	'https://github.com/csetea/magpieJS/blob/master/LICENSE.md'
 });
