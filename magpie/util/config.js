7fbab76d9b1e4766c5027209de11b985c2655c74
csetea <csetea@ancs-ubuntu>
2015-06-03 13:00:52 +0200

    API extended with 'merge' function

--- magpie/util/config.js
+++ magpie/util/config.js
@@ -4,23 +4,40 @@
  */
 define([], function() {
 	
+	function mergeArray(source, reference){
+		for (var p in reference) {
+			source.push(reference[p]);
+		}
+	}
 	
-	function mergeObject(source, reference){
-		
+	function mergeObject(source, reference, deep){
 		for (var p in reference) {
 			if ( typeof source[p] === "undefined") {
 				source[p] = reference[p];
 			}else if (reference[p]!== null && typeof reference[p] === "object" ){
-				mergeObject(source[p], reference[p]);
+				if (reference[p] instanceof Array){
+					mergeArray(source[p], reference[p]);
+				}else if (deep){
+					mergeObject(source[p], reference[p]);
+				}
 			}
 		}
-		
 	}
-
-	return function config(module, defaultConfig) {
+	
+	var API = function config(module, defaultConfig) {
 		/*jshint -W004 */ 
 		var config = module.config();
-		mergeObject(config, defaultConfig);
+		mergeObject(config, defaultConfig, true);
 		return config;
 	};
+	
+	API.merge= function(source, reference, deep){
+		if (reference instanceof Array){
+			mergeArray(source, reference);
+		}else{
+			mergeObject(source, reference, deep);
+		}
+	};
+	
+	return API;
 });
