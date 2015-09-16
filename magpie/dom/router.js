b0a60769540cc19c6a453d721773efc8f746918e
csetea <csetea@csetea-VPCSA>
2015-09-16 08:04:35 +0200

    !clean: remove deprecated router definition place

--- magpie/dom/router.js
+++
@@ -1,9 +0,0 @@
-/**
- * @URL https://github.com/csetea/magpieJS
- * @license MIT
- */
-define([ 'module', 'magpie/log!router' ], function(module, log) {
-	var config = module.config();
-//TODO add config for path matches and add viewNotFound and default ... for fallback
-
-});
