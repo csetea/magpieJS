a970ddd35fe6072b7879e8f39b9b027496f006b4
csetea <csetea@csetea-VPCSA>
2016-01-14 18:38:00 +0100

    fix: IE click - add timeout to perform click events under the selection list

--- magpie/html5/widget/select/m-select.js
+++ magpie/html5/widget/select/m-select.js
@@ -12,6 +12,38 @@ define([ 'magpie/log!magpie/html5/widget/select/m-select',
 		'css!./m-select.css' ], //
 function(log) {
 
+
+	var IE = (function () {
+	    "use strict";
+
+	    var ret, isTheBrowser,
+	        actualVersion,
+	        jscriptMap, jscriptVersion;
+
+	    isTheBrowser = false;
+	    jscriptMap = {
+	        "5.5": "5.5",
+	        "5.6": "6",
+	        "5.7": "7",
+	        "5.8": "8",
+	        "9": "9",
+	        "10": "10"
+	    };
+	    jscriptVersion = new Function("/*@cc_on return @_jscript_version; @*/")();
+
+	    if (jscriptVersion !== undefined) {
+	        isTheBrowser = true;
+	        actualVersion = jscriptMap[jscriptVersion];
+	    }
+
+	    ret = {
+	        isTheBrowser: isTheBrowser,
+	        actualVersion: actualVersion
+	    };
+
+	    return ret;
+	}());
+
 	var forEach = Array.prototype.forEach;
 
 	var findOptionEl = function(rt){
@@ -104,7 +136,13 @@ function(log) {
 						}
 					},50)
 				}else{
-					_this.close();
+					if (IE.isTheBrowser){
+						setTimeout(function(){
+							_this.close();
+						},100)
+					}else{
+						_this.close();
+					}
 				}
 			};
 
