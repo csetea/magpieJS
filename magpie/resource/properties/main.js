b905e8b632107002e71f912543f52d056d8998bb
csetea <csetea@csetea-VPCSA>
2016-05-02 14:19:43 +0200

    magpie/resource/porperties undefined/null check

--- magpie/resource/properties/main.js
+++ magpie/resource/properties/main.js
@@ -4,32 +4,34 @@
  */
 define([ 'magpie/log!magpie/resource/properties/main', 'magpie/util/config', 'module' ],
 			function(log, config, module) {
-	
+
 	// TODO doc config options
 	/*jshint -W004 */
 	var config =config(module,{
 		loader: 'text',
 		baseUri: ''
 	});
-		
+
 	var processProperties = function(text) {
-		var lines = text.split("\n");
 		var i, key;
 		var object = {};
-		lines.forEach(function(line) {
-			if (line.length && line.charAt(0) !== "#") {
-				i = line.indexOf("=");
-				key = line.substr(0, i).trim();
-				
-				//object[key] = (line.substr(i + 1).trim());
-				object[key] = unescape(
-					line.substr(i + 1).trim().replace(/\\u([\d\w]{4})/gi,
-						function (match, grp) {
-							return String.fromCharCode(parseInt(grp, 16)); 
-					} )
-				);
-			}
-		});
+		if (typeof text !== 'undefined' && text != null){
+			var lines = text.split("\n");
+			lines.forEach(function(line) {
+				if (line.length && line.charAt(0) !== "#") {
+					i = line.indexOf("=");
+					key = line.substr(0, i).trim();
+
+					//object[key] = (line.substr(i + 1).trim());
+					object[key] = unescape(
+						line.substr(i + 1).trim().replace(/\\u([\d\w]{4})/gi,
+							function (match, grp) {
+								return String.fromCharCode(parseInt(grp, 16));
+						} )
+					);
+				}
+			});			
+		}
 		return object;
 	};
 
